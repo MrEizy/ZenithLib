833493fc91efcb46cac8920ec4098dffe3d10f7f
MrEizy <MrEizy@hotmail.com>
2026-04-05 17:57:02 +0200

    Loads of changes

---
+++ src/main/java/net/zic/zenithlib/ZenithLib.java
@@ -0,0 +1,43 @@
+package net.zic.zenithlib;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(ZenithLib.MOD_ID)
+public class ZenithLib {
+    public static final String MOD_ID = "zenithlib";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public ZenithLib(IEventBus modEventBus, ModContainer modContainer) {
+
+        modEventBus.addListener(this::commonSetup);
+
+
+        NeoForge.EVENT_BUS.register(this);
+
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+        LOGGER.info("(!) Zenith Lib Connected (!)");
+    }
+}
