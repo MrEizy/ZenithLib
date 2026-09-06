186c5b6d847ea28b8f26958df2fb7014eed87497
Oliver <oliver.f.p.hesse@gmail.com>
2026-09-06 16:11:30 +0100

    Codec Helper for new ValueContainer

---
+++ src/main/java/net/zic/zenithlib/value_containers/typed/ValueContainerCodecHelper.java
@@ -0,0 +1,72 @@
+package net.zic.zenithlib.value_containers.typed;
+
+import com.mojang.datafixers.util.Either;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.EitherCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.animal.fish.Cod;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ValueContainerCodecHelper {
+    public record BaseValue<T extends Number>(Identifier container,T value){
+        public BaseValue(Map.Entry<Identifier,T> entry){
+            this(entry.getKey(),entry.getValue());
+        }
+
+    }
+
+    public static <T extends Number> Codec<List<BaseValue<T>>> baseValueListCodec(Codec<T> valCodec){
+        return Codec.unboundedMap(
+                Identifier.CODEC,
+                valCodec
+        ).xmap(
+                raw->raw.entrySet().stream().map(BaseValue::new).toList(),
+                list->list.stream().collect(Collectors.toMap(BaseValue::container,BaseValue::value))
+        );
+    }
+
+    public static <T extends Number> Codec<BonusModifier<T>> bonusModifierCodec(Codec<T> valCodec){
+        return RecordCodecBuilder.create(
+                instance->instance.group(
+                        Codec.STRING.validate(
+                                val-> val.equals("bonus") ? DataResult.success(val) : DataResult.error(()->"expected multiplier")
+                        ).fieldOf("type").forGetter((val)->"bonus"),
+                        Identifier.CODEC.fieldOf("id").forGetter(BonusModifier::id),
+                        Identifier.CODEC.optionalFieldOf("group",Identifier.parse("none")).forGetter(BonusModifier::group),
+                        Codec.INT.optionalFieldOf("operation_group",0).forGetter(BonusModifier::operationGroup),
+                        valCodec.fieldOf("value").forGetter(BonusModifier<T>::val)
+                ).apply(instance,BonusModifier::new)
+        );
+    }
+    public static Codec<MultiplierModifier> multiplierModifierCodec(){
+        return RecordCodecBuilder.create(
+                instance->instance.group(
+                        Codec.STRING.validate(
+                                val-> val.equals("multiplier") ? DataResult.success(val) : DataResult.error(()->"expected multiplier")
+                        ).fieldOf("type").forGetter((val)->"multiplier"),
+                        Identifier.CODEC.fieldOf("id").forGetter(MultiplierModifier::id),
+                        Identifier.CODEC.optionalFieldOf("group",Identifier.parse("none")).forGetter(MultiplierModifier::group),
+                        Codec.INT.optionalFieldOf("operation_group",0).forGetter(MultiplierModifier::operationGroup),
+                        Codec.DOUBLE.fieldOf("value").forGetter(MultiplierModifier::val)
+                ).apply(instance,MultiplierModifier::new)
+        );
+    }
+
+
+    public static <T extends Number> Codec<Either<BonusModifier<T>,MultiplierModifier>> modifierCodec(Codec<T> valCodec){
+        return Codec.either(bonusModifierCodec(valCodec),multiplierModifierCodec());
+    }
+
+    public static <T extends Number> Codec<Map<Identifier,List<Either<BonusModifier<T>,MultiplierModifier>>>> containerModifiers(Codec<T> valCodec){
+
+        return Codec.unboundedMap(Identifier.CODEC,modifierCodec(valCodec).listOf());
+    }
+}
